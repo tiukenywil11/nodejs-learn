fa599299a7768afef15163792ee6111be5afac08
Kenywil Tiu <tiukenwil11@gmail.com>
2022-05-12 13:53:58 +0800

    [ethers] Changed Infura to Rinkeby node

--- ethersjs/ethers_demo/ethers_demo.js
+++ ethersjs/ethers_demo/ethers_demo.js
@@ -3,13 +3,16 @@ const { ethers } = require("ethers");
 
 // Create connection to Ether RPC
 const INFURA_ID = process.env.INFURA_ID
-const eth_provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+const eth_provider = new ethers.providers.JsonRpcProvider(`https://rinkeby.infura.io/v3/${INFURA_ID}`)
 
 const my_address = '0xe33Cf08406FDB46EaEAA9cC702FDAd54a076C709';
 
+// Get ETH balance of wallet address
 const balance_fn  = async() => {
     const balance = await eth_provider.getBalance(my_address);
     console.log(`ETH balance of ${my_address}: ${ethers.utils.formatEther(balance)} ETH`);
 }
 
+
 balance_fn();
+
